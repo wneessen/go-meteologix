e3756a5466c6bb8ed039b9d6f1d80bc601c71198
Winni Neessen <winfried.neessen@cleverbridge.com>
2023-06-27 19:01:37 +0200

    Update comment for DateTime function in radiation.go
    The comment for the DateTime function in radiation.go was updated to more accurately describe its functionality. The previous version of the comment suggested that the function returns a boolean value indicating the availability of a radiation value at the time of a query. However, the function actually returns a time.Time object representing the date and time of the query. The comment was thus updated to reflect this.

--- radiation.go
+++ radiation.go
@@ -20,8 +20,8 @@ func (r Radiation) IsAvailable() bool {
 	return !r.na
 }
 
-// DateTime returns true if an Radiation value was
-// available at time of query
+// DateTime returns the time.Time object representing the date and time
+// at which the Radiation value was queried
 func (r Radiation) DateTime() time.Time {
 	return r.dt
 }
