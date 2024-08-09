8bb2fd2616935a5b6b43fcdaba3213174c6498ab
Winni Neessen <wn@neessen.net>
2024-08-09 22:09:05 +0200

    Refactor variable names for readability
    Updated variable names for clarity and consistency across the HTTP client functions. This improves code readability by making the code more self-explanatory and easier to maintain, especially when dealing with HTTP requests and responses.

--- httpclient.go
+++ httpclient.go
@@ -27,8 +27,8 @@ const (
 	MIMETypeJSON = "application/json"
 )
 
-// ErrNonJSONResponse is returned when a HTTPClient request did not return the
-// expected application/json content type
+// ErrNonJSONResponse is returned when a HTTPClient request did not return the expected
+// application/json content type
 var ErrNonJSONResponse = errors.New("HTTP response is of non-JSON content type")
 
 // HTTPClient is a type wrapper for the Go stdlib http.Client and the Config
@@ -47,113 +47,113 @@ type APIError struct {
 }
 
 // NewHTTPClient returns a new HTTP client
-func NewHTTPClient(c *Config) *HTTPClient {
-	tc := &tls.Config{
+func NewHTTPClient(config *Config) *HTTPClient {
+	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS12,
 	}
-	ht := &http.Transport{TLSClientConfig: tc}
-	hc := &http.Client{
+	httpTransport := &http.Transport{TLSClientConfig: tlsConfig}
+	httpClient := &http.Client{
 		Timeout:   HTTPClientTimeout,
-		Transport: ht,
+		Transport: httpTransport,
 	}
-	return &HTTPClient{c, hc}
+	return &HTTPClient{config, httpClient}
 }
 
 // Get performs a HTTP GET request for the given URL with the default HTTP timeout
-func (hc *HTTPClient) Get(u string) ([]byte, error) {
-	return hc.GetWithTimeout(u, HTTPClientTimeout)
+func (hc *HTTPClient) Get(url string) ([]byte, error) {
+	return hc.GetWithTimeout(url, HTTPClientTimeout)
 }
 
-// GetWithTimeout performs a HTTP GET request for the given URL and sets a timeout
-// context with the given timeout duration
-func (hc *HTTPClient) GetWithTimeout(u string, t time.Duration) ([]byte, error) {
-	ctx, cfn := context.WithTimeout(context.Background(), t)
-	defer cfn()
-	hr, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
+// GetWithTimeout performs a HTTP GET request for the given URL and sets a timeout context
+// with the given timeout duration
+func (hc *HTTPClient) GetWithTimeout(url string, timeout time.Duration) ([]byte, error) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), timeout)
+	defer cancelFunc()
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
-	hr.Header.Set("User-Agent", hc.userAgent)
-	hr.Header.Set("Content-Type", MIMETypeJSON)
-	hr.Header.Set("Accept", MIMETypeJSON)
-	hr.Header.Set("Accept-Language", hc.acceptLang)
+	request.Header.Set("User-Agent", hc.userAgent)
+	request.Header.Set("Content-Type", MIMETypeJSON)
+	request.Header.Set("Accept", MIMETypeJSON)
+	request.Header.Set("Accept-Language", hc.acceptLang)
 
 	// User authentication (only required for Meteologix API calls)
-	if strings.HasPrefix(u, APIBaseURL) {
-		hc.setAuthentication(hr)
+	if strings.HasPrefix(url, APIBaseURL) {
+		hc.setAuthentication(request)
 	}
 
-	sr, err := hc.Do(hr)
+	response, err := hc.Do(request)
 	if err != nil {
 		return nil, err
 	}
-	if sr == nil {
+	if response == nil {
 		return nil, errors.New("nil response received")
 	}
-	defer func(b io.ReadCloser) {
-		if err = b.Close(); err != nil {
+	defer func(body io.ReadCloser) {
+		if err = body.Close(); err != nil {
 			log.Printf("failed to close HTTP request body: %s", err)
 		}
-	}(sr.Body)
+	}(response.Body)
 
-	if !strings.HasPrefix(sr.Header.Get("Content-Type"), MIMETypeJSON) {
+	if !strings.HasPrefix(response.Header.Get("Content-Type"), MIMETypeJSON) {
 		return nil, ErrNonJSONResponse
 	}
-	if sr.StatusCode >= http.StatusBadRequest {
-		ae := new(APIError)
-		if err = json.NewDecoder(sr.Body).Decode(ae); err != nil {
+	if response.StatusCode >= http.StatusBadRequest {
+		apiError := new(APIError)
+		if err = json.NewDecoder(response.Body).Decode(apiError); err != nil {
 			return nil, fmt.Errorf("failed to unmarshal error JSON: %w", err)
 		}
-		if ae.Code < 1 {
-			ae.Code = sr.StatusCode
+		if apiError.Code < 1 {
+			apiError.Code = response.StatusCode
 		}
-		if ae.Details == "" {
-			ae.Details = sr.Status
+		if apiError.Details == "" {
+			apiError.Details = response.Status
 		}
-		return nil, *ae
+		return nil, *apiError
 	}
 
-	buf := &bytes.Buffer{}
-	bw := bufio.NewWriter(buf)
-	_, err = io.Copy(bw, sr.Body)
+	buffer := &bytes.Buffer{}
+	bufferWriter := bufio.NewWriter(buffer)
+	_, err = io.Copy(bufferWriter, response.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to copy HTTP response body to buffer: %w", err)
 	}
-	if err = bw.Flush(); err != nil {
+	if err = bufferWriter.Flush(); err != nil {
 		return nil, fmt.Errorf("failed to flush buffer: %w", err)
 	}
-	return buf.Bytes(), nil
+	return buffer.Bytes(), nil
 }
 
 // setAuthentication sets the corresponding user authentication header. If an API Key is set, this
 // will be preferred, alternatively a username/authPass combination for HTTP Basic auth can
 // be used
-func (hc *HTTPClient) setAuthentication(hr *http.Request) {
+func (hc *HTTPClient) setAuthentication(httpRequest *http.Request) {
 	if hc.apiKey != "" {
-		hr.Header.Set("X-API-Key", hc.Config.apiKey)
+		httpRequest.Header.Set("X-API-Key", hc.Config.apiKey)
 		return
 	}
 	if hc.bearerToken != "" {
-		hr.Header.Set("Authorization", "Bearer"+hc.bearerToken)
+		httpRequest.Header.Set("Authorization", "Bearer"+hc.bearerToken)
 		return
 	}
 	if hc.authUser != "" && hc.authPass != "" {
-		hr.SetBasicAuth(url.QueryEscape(hc.authUser), url.QueryEscape(hc.authPass))
+		httpRequest.SetBasicAuth(url.QueryEscape(hc.authUser), url.QueryEscape(hc.authPass))
 	}
 }
 
 // Error satisfies the error interface for the APIError type
 func (e APIError) Error() string {
-	var em strings.Builder
-	em.WriteString("API request failed with status HTTP ")
-	em.WriteString(fmt.Sprintf("%d: ", e.Code))
+	var errorMsg strings.Builder
+	errorMsg.WriteString("API request failed with status HTTP ")
+	errorMsg.WriteString(fmt.Sprintf("%d: ", e.Code))
 	if e.Details != "" {
-		em.WriteString(e.Details)
+		errorMsg.WriteString(e.Details)
 	}
 	if e.Message != "" {
-		em.WriteString(" (Optional message: ")
-		em.WriteString(e.Message)
-		em.WriteString(")")
+		errorMsg.WriteString(" (Optional message: ")
+		errorMsg.WriteString(e.Message)
+		errorMsg.WriteString(")")
 	}
-	return em.String()
+	return errorMsg.String()
 }
