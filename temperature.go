5e8ae447efdc23c73082769e7d7d7b346b3bc63e
Winni Neessen <wn@neessen.net>
2024-08-09 22:31:59 +0200

    Refactor code comments for better readability
    Simplified comments by refining sentence structures and consolidating lines. This improves readability and maintains consistency across the codebase. No functionality changes were made.

--- temperature.go
+++ temperature.go
@@ -10,25 +10,22 @@ import (
 	"time"
 )
 
-// Temperature is a type wrapper of an WeatherData for holding temperature
-// values in WeatherData
+// Temperature is a type wrapper of an WeatherData for holding temperature values in WeatherData
 type Temperature WeatherData
 
-// IsAvailable returns true if an Temperature value was
-// available at time of query
+// IsAvailable returns true if an Temperature value was available at time of query
 func (t Temperature) IsAvailable() bool {
 	return !t.notAvailable
 }
 
-// DateTime returns the time at which the temperature data was
-// generated or fetched
+// DateTime returns the time at which the temperature data was generated or fetched
 func (t Temperature) DateTime() time.Time {
 	return t.dateTime
 }
 
 // Value returns the float64 value of an Temperature
-// If the Temperature is not available in the WeatherData
-// Vaule will return math.NaN instead.
+//
+// If the Temperature is not available in the WeatherData, Value will return math.NaN instead.
 func (t Temperature) Value() float64 {
 	if t.notAvailable {
 		return math.NaN()
@@ -37,6 +34,7 @@ func (t Temperature) Value() float64 {
 }
 
 // Source returns the Source of an Temperature
+//
 // If the Source is not available it will return SourceUnknown
 func (t Temperature) Source() Source {
 	return t.source
@@ -52,8 +50,7 @@ func (t Temperature) Celsius() float64 {
 	return t.floatVal
 }
 
-// CelsiusString returns the Temperature value as Celsius
-// formated string.
+// CelsiusString returns the Temperature value as Celsius formated string.
 //
 // This is an alias for the fmt.Stringer interface
 func (t Temperature) CelsiusString() string {
@@ -65,8 +62,7 @@ func (t Temperature) Fahrenheit() float64 {
 	return t.floatVal*9/5 + 32
 }
 
-// FahrenheitString returns the Temperature value as Fahrenheit
-// formated string.
+// FahrenheitString returns the Temperature value as Fahrenheit formated string.
 func (t Temperature) FahrenheitString() string {
 	return fmt.Sprintf("%.1f°F", t.Fahrenheit())
 }
