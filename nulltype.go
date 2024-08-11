bc6916d6736ac124434a981184c52cef9c1d5bba
Winni Neessen <wn@neessen.net>
2024-08-11 21:17:10 +0200

    Add support for nullable generic types
    Introduce a generic `Variable` type to handle nullable values, including specialized types `NilInt`, `NilFloat64`, and `NilString`. Implement `Get`, `NotNull`, and `Reset` methods, along with custom JSON unmarshalling to differentiate between null and non-null values.

---
+++ nulltype.go
@@ -0,0 +1,45 @@
+package meteologix
+
+import "encoding/json"
+
+// Variable is a generic variable type that can be null.
+type Variable[T any] struct {
+	value  T
+	notNil bool
+}
+
+// Get the value of the Variable
+func (f *Variable[T]) Get() T {
+	return f.value
+}
+
+// NotNull returns true when a Variable is not null
+func (f *Variable[T]) NotNull() bool {
+	return f.notNil
+}
+
+// Reset resets the value to the Variable to a zero value and sets it to be nil
+func (f *Variable[T]) Reset() {
+	var newVal T
+	f.value = newVal
+	f.notNil = false
+}
+
+// NilInt is an int type that can be nil
+type NilInt = Variable[int]
+
+// NilFloat64 is an float64 type that can be nil
+type NilFloat64 = Variable[float64]
+
+// NilString is a string type that can be nil
+type NilString = Variable[string]
+
+// UnmarshalJSON interprets the generic Nil types and sets the value and notnil of the type
+func (f *Variable[T]) UnmarshalJSON(data []byte) error {
+	if string(data) != "null" {
+		f.value = *new(T)
+		f.notNil = true
+		return json.Unmarshal(data, &f.value)
+	}
+	return nil
+}
